de7ed6267cf9a83a39f053c52270478ef1a8b228
clarence112 <clarencepacelli@gmail.com>
2023-08-21 04:13:02 -0400

    add the basics

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,60 @@
 extends CharacterBody3D
+class_name Player
 
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+static var instance:Player
+
+
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 @onready var cam:Camera3D = $Camera3D
+@onready var ray:RayCast3D = $Camera3D/RayCast3D
+@onready var parent:Node3D = $"../"
+@onready var grab:Node3D = $Camera3D/grabpoint
+
+
+func _ready() -> void:
+    instance = self
 
 
 func _input(event: InputEvent) -> void:
     if event is InputEventMouseMotion:
-        rotate_y(event.relative.x / -300)
-        cam.rotate_x(event.relative.y / -300)
-        cam.rotation_degrees.x = clampf(cam.rotation_degrees.x, -90, 90)
+        if Input.get_action_raw_strength("rotate") > 0.1:
+            if grab.get_child_count() > 0:
+                grab.get_child(0).apply_torque(Vector3(event.relative.y, event.relative.x, 0) / 10)
+        else:
+            rotate_y(event.relative.x / -300)
+            cam.rotate_x(event.relative.y / -300)
+            cam.rotation_degrees.x = clampf(cam.rotation_degrees.x, -90, 90)
+    elif event.is_action_pressed("interact"):
+        if grab.get_child_count() > 0:
+            var obj = grab.get_child(0)
+            var trans = Transform3D(obj.global_transform)
+            grab.remove_child(obj)
+            obj.axis_lock_linear_x = false
+            obj.axis_lock_linear_y = false
+            obj.axis_lock_linear_z = false
+            obj.collision_layer = 0b1
+            obj.collision_mask = 0b1
+            parent.add_child(obj)
+            obj.global_transform = trans
+        elif ray.is_colliding():
+            var obj = ray.get_collider()
+            if obj.is_in_group("grab"):
+                obj.get_parent().remove_child(obj)
+                grab.add_child(obj)
+                obj.linear_velocity = Vector3.ZERO
+                obj.position = Vector3.ZERO
+                obj.axis_lock_linear_x = true
+                obj.axis_lock_linear_y = true
+                obj.axis_lock_linear_z = true
+                obj.collision_layer = 0b0
+                obj.collision_mask = 0b0
 
 
 func _physics_process(delta: float) -> void:
@@ -37,5 +76,8 @@ func _physics_process(delta: float) -> void:
     else:
         velocity.x = move_toward(velocity.x, 0, SPEED)
         velocity.z = move_toward(velocity.z, 0, SPEED)
+
+    if grab.get_child_count() > 0:
+        grab.get_child(0).position = Vector3.ZERO
 
     move_and_slide()
---
+++ scripts/portal.gd
@@ -0,0 +1,33 @@
+extends RigidBody3D
+class_name Portal
+
+
+var vp:Viewport
+var svp:SubViewport
+var transformer:Node3D
+var other:Portal
+var cam:Camera3D
+
+
+func _ready() -> void:
+    vp = get_viewport()
+    vp.size_changed.connect(resize)
+    svp = $SubViewport
+    transformer = $"Node3D"
+    if name == "orange":
+        other = $"../blue"
+    else:
+        other = $"../orange"
+    cam = $SubViewport/Camera3D
+
+
+func resize():
+    svp.size = vp.get_visible_rect().size
+
+
+func _process(_delta):
+    transformer.global_transform = Player.instance.cam.global_transform
+    transformer.position *= Vector3(-1, 1, -1)
+    transformer.rotation_degrees.y += 180
+    other.transformer.transform = transformer.transform
+    other.cam.global_transform = other.transformer.global_transform
